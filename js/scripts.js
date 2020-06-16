cff20a34f8f398a079b5c8c1e73c1b885af6264e
Sean Downs <spdowns@protonmail.com>
2020-06-16 14:24:57 -0700

    add a select element on html with four movie options. deciding next how to manage times for selected films
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
-function Movie (name, ) {
-  this.name =,
-  timesAvailable,
-  isFirstRelease
+function Movie(name, isFirstRelease) {
+  this.name = name,
+  this.timesAvailable = [],
+  this.isFirstRelease = isFirstRelease
 }
+
+Movie.prototype.addMovieTime = function(time) {
+  this.timesAvailable.push(time);
+}
+
+let stella = new Movie("stella", false);
+stella.addMovieTime("12pm");
+stella.addMovieTime("1pm");
